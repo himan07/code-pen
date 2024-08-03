c8c1906806cb2fead0194ced641efca99d1b99ec
Himanshu yadav <himanshuyadav@Himanshus-Laptop.local>
2024-08-03 19:49:19 +0530

    done code-pen-clone

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,52 @@
-import React, { useState } from "react";
-import Editor from "./Editor";
-import "../index.css"
+import React, { useState, useEffect } from 'react';
+import Editor from './Editor'
+import useLocalStorage from '../hooks/useLocalStorage'
 
-const App = () => {
-  const [html, setHtml] = useState("");
-  const [css, setCss] = useState("");
-  const [js, setJs] = useState("");
+function App() {
+  const [html, setHtml] = useLocalStorage('html', '')
+  const [css, setCss] = useLocalStorage('css', '')
+  const [js, setJs] = useLocalStorage('js', '')
+  const [srcDoc, setSrcDoc] = useState('')
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
+        <html>
+          <body>${html}</body>
+          <style>${css}</style>
+          <script>${js}</script>
+        </html>
+      `)
+    }, 250)
+
+    return () => clearTimeout(timeout)
+  }, [html, css, js])
 
   return (
     <>
       <div className="pane top-pane">
-        <Editor language="xml" displayName="HTML" value={html} onChange={setHtml} />
-        <Editor language="css" displayName="CSS" value={css} onChange={setCss} />
-        <Editor language="javascript" displayName="JS" value={js} onChange={setJs} />
-        
+        <Editor
+          language="xml"
+          displayName="HTML"
+          value={html}
+          onChange={setHtml}
+        />
+        <Editor
+          language="css"
+          displayName="CSS"
+          value={css}
+          onChange={setCss}
+        />
+        <Editor
+          language="javascript"
+          displayName="JS"
+          value={js}
+          onChange={setJs}
+        />
       </div>
       <div className="pane">
         <iframe
+          srcDoc={srcDoc}
           title="output"
           sandbox="allow-scripts"
           frameBorder="0"
@@ -25,7 +55,7 @@ const App = () => {
         />
       </div>
     </>
-  );
-};
+  )
+}
 
 export default App;
